d06072795d49214bbd30359e094fb150b7c27a34
Aaditya Prajapat <aadi1737ug@gmail.com>
2026-06-19 19:23:55 +0530

    Async Added

--- src/main/java/com/aadityaJi/AnchorPay/Config/EmailService.java
+++ src/main/java/com/aadityaJi/AnchorPay/Config/EmailService.java
@@ -3,8 +3,10 @@ package com.aadityaJi.AnchorPay.Config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+@Async
 @Service
 public class EmailService {
 
